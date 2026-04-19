ecd0072b7a44f331ed21bfa9a7251bca815e8015
emiliaalzararee <emilia.al-zararee@outlook.com>
2026-04-19 19:10:46 +0300

    jotain

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const post = require('../models/Post');
-const Post = require('../models/Post');
+const post = require('../../models/Post');
+const Post = require('../../models/Post');
 
 const router = express.Router();
 
