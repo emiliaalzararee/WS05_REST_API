691dd0c1d520864abedab0a2c0de670144943bbd
emiliaalzararee <emilia.al-zararee@outlook.com>
2026-04-19 19:23:52 +0300

    fixed a file path

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const post = require('../../models/Post');
-const Post = require('../../models/Post');
 
+const Post = require('../models/Post');
 const router = express.Router();
 
 function isValidObjectId(id) {
